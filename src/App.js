12cf42946f599ba4722c3eaf681a3950f697901c
Arlen Mendes <arlenmendes@ioasys.com.br>
2019-07-15 11:33:23 -0300

    added lib and functions.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Ws from '@adonisjs/websocket-client'
+const ws = Ws('ws://192.168.0.19:3333')
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    ws.connect()
+
+    ws.on('open', () => {
+      const notifications = ws.subscribe('notifications')
+
+      notifications.on('rdo', (data) => {
+        console.log(data)
+      })
+
+      notifications.on('abnormality', (data) => {
+        console.log(data)
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Arlens
+      </div>
+    );
+  }
+  
 }
 
 export default App;
